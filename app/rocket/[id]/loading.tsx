0051077cfded49dfc096f29b6934d422a506a151
dgouron <damien.gouron@gmail.com>
2023-01-06 15:17:58 +0100

    Modify loader and center it

--- app/rocket/[id]/loading.tsx
+++ app/rocket/[id]/loading.tsx
@@ -1,8 +1,9 @@
 import LoadingSpinner from "../../../src/components/loader/LoadingSpinner";
+import styles from "../../../src/components/loader/LoadingSpinner.module.css";
 
 export default function Loading() {
   return (
-    <section>
+    <section className={styles.loading__container}>
       <h2>Rocket loading...</h2>
       <LoadingSpinner />
     </section>
