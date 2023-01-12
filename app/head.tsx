590019612da6c7047acfa467dc76c118c1c564f3
dgouron <damien.gouron@gmail.com>
2023-01-12 16:01:58 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,14 @@
 export default function Head() {
   return (
     <>
-      <title>SpaceX rocket information</title>
+      <title>SpaceX rocket informations</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
         content="A SpaceX rocket information application"
       />
+      <meta name="author" content="Damien Gouron" />
+      <meta name="language" content="English" />
       <link rel="icon" href="/favicon.ico" />
     </>
   );
